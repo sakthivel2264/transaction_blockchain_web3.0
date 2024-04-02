0c64c685288b86a9b343fd7cdd1356226f1c7317
SakthivelPandiyan <142210459+sakthivel2264@users.noreply.github.com>
2024-04-02 17:47:38 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks:{
     sepolia: {
       url: 'https://eth-sepolia.g.alchemy.com/v2/C4TMa-c5X4SRDwVp_lMRa-nXpbn8xvR5',
-      accounts: ['5e04c50182140cae76bae0419c75cec352006221d1ce1df4d708096673661739']
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
   }
 }
